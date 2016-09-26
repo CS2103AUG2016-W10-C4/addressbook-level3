bcafcc702fb86de02d5da0aa9be63a4f29ff9ff7
Xien Dong Wang <xdrawks@gmail.com>
2016-09-26 21:32:58 +0800

    Updates FindCommand class annotation to case insensitive

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
