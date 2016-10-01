dc9cc668fa06070e80103d6cf9d47d8fb200dab5
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-01 20:56:54 +0800

    clean up style

--- src/seedu/addressbook/data/CommandLog.java
+++ src/seedu/addressbook/data/CommandLog.java
@@ -11,18 +11,13 @@ import seedu.addressbook.commands.Command;
 /**
  * Storage file which stores all the commands executed by user.
  * Useful for undoing a command later. 
- * 
- * Guarantees
- * -  Every mutable command executed can be found. 
- * @author Govind
- *
  */
 public class CommandLog {
     private  ArrayList<String> commandsEntered;
-    private static final String LOGGED_COMMAND_MESSAGE="Time:%1$s| User %2$s";
+    private static final String LOGGED_COMMAND_MESSAGE = "Time:%1$s| User %2$s";
     
-    public CommandLog(){
-        commandsEntered=new ArrayList<String>();
+    public CommandLog() {
+        commandsEntered = new ArrayList<String>();
     }
     
     /**
@@ -39,21 +34,17 @@ public class CommandLog {
         return commandsEntered;
     }
     
-    
     public static String getCurrentTime() {
         return DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneId.of("GMT")));
     }
     
     /**
-     * Logs a command (which changes the addressbook in some way)
+     * Logs a mutating AddressBook command
      * @param command
      */
-    public void log(Command command){
+    public void log(Command command) {
         String actionDone;
-        actionDone=String.format(LOGGED_COMMAND_MESSAGE, getCurrentTime(), command.getExecutedAction());
+        actionDone = String.format(LOGGED_COMMAND_MESSAGE, getCurrentTime(), command.getExecutedAction());
         commandsEntered.add(actionDone);
     }
-    
-    
-    
 }
